ffe0ad6eb2530b7eee23497568c470e23bb1a045
Danang Tri Atmaja <114817148+dword32bit@users.noreply.github.com>
2025-01-28 18:27:42 +0700

    Update reverseshell_installer.sh

--- reverseshell_installer.sh
+++ reverseshell_installer.sh
@@ -1,1 +1,18 @@
-.
+#!/bin/bash
+
+URL="https://github.com/dword32bit/ReverseShellBuilder/releases/download/1.0/rebuild"
+DEST="/usr/bin/rebuild"
+
+echo "Downloading ReverseShellBuilder..."
+curl -L $URL -o $DEST
+
+
+
+echo "Making the file executable..."
+chmod +x $DEST
+
+if [ -f "$DEST" ]; then
+    echo "ReverseShellBuilder installed successfully at $DEST"
+else
+    echo "Installation failed."
+fi
