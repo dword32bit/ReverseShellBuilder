583c251c8e5b51a5763d4aba7405e6ffb52ee1ee
Danang Tri Atmaja <114817148+dword32bit@users.noreply.github.com>
2025-01-28 18:37:30 +0700

    Update reverseshell_installer.sh

--- reverseshell_installer.sh
+++ reverseshell_installer.sh
@@ -3,10 +3,24 @@
 URL="https://github.com/dword32bit/ReverseShellBuilder/releases/download/1.0/rebuild"
 DEST="/usr/bin/rebuild"
 
+REQUIRED_TOOLS=("xterm" "netcat" "ncat" "socat" "busybox" "rustcat" "pwncat")
+
+check_and_install_tools() {
+    for tool in "${REQUIRED_TOOLS[@]}"; do
+        if ! command -v "$tool" &>/dev/null; then
+            echo "$tool is not installed. Installing..."
+            sudo apt-get install -y "$tool"
+        else
+            echo "$tool is already installed."
+        fi
+    done
+}
+
+check_and_install_tools
+pip install colorama --break-system-packages
+
 echo "Downloading ReverseShellBuilder..."
 curl -L $URL -o $DEST
-
-
 
 echo "Making the file executable..."
 chmod +x $DEST
