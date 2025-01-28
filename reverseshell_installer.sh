eef059b3b1602e7c96093eaa4f01d82331338e73
Danang Tri Atmaja <114817148+dword32bit@users.noreply.github.com>
2025-01-28 18:41:36 +0700

    Update reverseshell_installer.sh

--- reverseshell_installer.sh
+++ reverseshell_installer.sh
@@ -5,6 +5,18 @@ DEST="/usr/bin/rebuild"
 
 REQUIRED_TOOLS=("xterm" "netcat" "ncat" "socat" "busybox" "rustcat" "pwncat")
 
+# Function to check if the script is running as root
+check_if_root() {
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "This script must be run as root. Please use sudo."
+        exit 1
+    fi
+}
+
+# Check if the script is running as root
+check_if_root
+
+# Function to check and install required tools
 check_and_install_tools() {
     for tool in "${REQUIRED_TOOLS[@]}"; do
         if ! command -v "$tool" &>/dev/null; then
@@ -16,15 +28,21 @@ check_and_install_tools() {
     done
 }
 
+# Install required tools
 check_and_install_tools
+
+# Install colorama via pip
 pip install colorama --break-system-packages
 
+# Download ReverseShellBuilder
 echo "Downloading ReverseShellBuilder..."
 curl -L $URL -o $DEST
 
+# Make the file executable
 echo "Making the file executable..."
 chmod +x $DEST
 
+# Verify the installation
 if [ -f "$DEST" ]; then
     echo "ReverseShellBuilder installed successfully at $DEST"
 else
